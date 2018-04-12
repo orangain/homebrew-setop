b7e4c04e6aada90bb96cf435ca2fcaa787f028d5
orangain <orangain@gmail.com>
2018-04-12 22:08:58 +0900

    Mark help2man dependency though it does not work well

--- setop.rb
+++ setop.rb
@@ -3,7 +3,7 @@ class Setop < Formula
   homepage "https://github.com/phisigma/setop"
   url "https://github.com/phisigma/setop/archive/v0.1.tar.gz"
   sha256 "ad5138e635607abaad871c1c068de3684113f7187dccf8652a5c66cd2eca8d07"
-  #depends_on "help2man" => :build
+  depends_on "help2man" => :build
 
   def install
     system "make", "install", "BIN=#{bin}", "HELP=#{man1}"
